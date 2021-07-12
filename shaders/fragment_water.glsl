abe91b75cf79e62ba3d4f1bf1a13fc234224bf33
Alexandre Bourquelot <alexandre.bourquelot@epita.fr>
2021-07-12 11:37:09 +0200

    dégradé pour l'eau

--- shaders/fragment_water.glsl
+++ shaders/fragment_water.glsl
@@ -1,15 +1,31 @@
 #version 450
 
 out vec4 FragColor;
+in float distToCamera;
 
 void main()
 {
-//        FragColor = vec4(vec3(200.0f / 255.0f, 0.0f / 255.0f, 0.0f / 255.0f), 1.0f);
-//
-        FragColor = vec4(vec3(10.0f / 255.0f, 98.0f / 255.0f, 224.0f / 255.0f), 1.0f);
-    /*
-    FragColor = vec4(vec3(texture(texture_diffuse1, TexCoords)) + color, 1.0f);
-    FragColor = mix(texture(texture_diffuse1, TexCoords), vec4(color, 1.0f), 0.8f);
-    */
+        vec3 colFar = vec3(84.0f / 255.0f, 124.0f / 255.0f, 252.0f / 255.0f);
+        vec3 colClose = vec3(10.0f / 255.0f, 98.0f / 255.0f, 224.0f / 255.0f);
+        vec3 colInter = mix(colClose, colFar, 0.5);
 
+        int distFar2 = 400;
+        int distFar1 = 300;
+
+        if (distToCamera > distFar2)
+        {
+                FragColor = vec4(colFar, 1.0f);
+        }
+        else if (distToCamera > distFar1)
+        {
+                float fac = (distToCamera - distFar1) / (distFar2 - distFar1);
+                fac = clamp(fac, 0, 1);
+                FragColor = vec4(mix(colInter, colFar, fac), 1.0f);
+        }
+        else
+        {
+                float fac = distToCamera / distFar1;
+                fac = clamp(fac, 0, 1);
+                FragColor = vec4(mix(colClose, colInter, fac), 1.0f);
+        }
 }
--- shaders/vertex_water.glsl
+++ shaders/vertex_water.glsl
@@ -4,6 +4,7 @@ layout (location = 0) in vec3 aPos;
 
 
 out vec2 TexCoords;
+out float distToCamera;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -11,5 +12,7 @@ uniform mat4 projection;
 
 void main()
 {
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    vec4 cs_position = view * model * vec4(aPos, 1.0);
+    distToCamera = -cs_position.z;
+    gl_Position = projection * cs_position;
 }
